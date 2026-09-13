7ea013bacaa6327b6d08dcd072e334c1ccbf63c1
sydomori <sydneyomori@gmail.com>
2026-09-14 01:06:35 +0300

    feat: define verify_password function

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -18,3 +18,15 @@ def hash_password(
     """hash a pain text password using bcrypt and return a utf-8 encoded string for storage"""
     hashed = bcrypt.hashpw(plain_password.encode("utf-8"), bcrypt.gensalt())
     return hashed.decode("utf-8")
+
+def verify_password(
+    plain_password:str,
+    password_hash:str
+) -> bool:
+    """Checks plaintext password against password hash"""
+    if not plain_password or not password_hash:
+        return False
+    try:
+        return bcrypt.checkpw(plain_password.encode("utf-8"), password_hash.encode("utf-8"))
+    except ValueError:
+        return False
