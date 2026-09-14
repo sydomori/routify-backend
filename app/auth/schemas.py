44318c84e0eb0e9f08a4f2d09add109a59a18b03
sydomori <sydneyomori@gmail.com>
2026-09-14 21:32:06 +0300

    feat: create BootstrapManagerSchema

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -22,6 +22,11 @@ class OnboardDriverSchema(Schema):
     name = fields.Str(required=True, validate=validate.Length(min=1, max=128))
     phone = fields.Str(required=True, validate=validate.Length(min=8, max=20))
 
+class BootstrapManagerSchema(Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=1, max=120))
+    phone = fields.Str(required=True, validate=validate.Length(min=8, max=20))
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, load_only=True, validate=validate.Length(min=8))
 
 class LoginSchema(Schema):
     identifier = fields.Str(required=True) # phone for drivers, email for managers
