b8025f50aed836886cd009b3dc7596e99fe3d018
sydomori <sydneyomori@gmail.com>
2026-09-14 09:50:35 +0300

    feat: create UserPublicSchema

---
+++ app/auth/schemas.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields, validate
+from app.auth.models import User
+
+class UserPublicSchema(Schema):
+    """
+        Schema for serializing public user information.
+        set dump_only as they are only used as output only (serialize a user object to JSON)
+        is purely used to shape what goes out over the API
+    """
+    id = fields.Int(dump_only=True)
+    name = fields.Str(dump_only=True)
+    phone = fields.Str(dump_only=True)
+    email = fields.Str(dump_only=True, allow_none=True)
+    role = fields.Str(dump_only=True)
+    must_change_password = fields.Bool(dump_only=True)
+    driver_status = fields.Str(dump_only=True, allow_none=True)
+    is_active = fields.Bool(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
