2104a4427ab732377ff93aa30e7b8e28989d8773
sydomori <sydneyomori@gmail.com>
2026-09-14 09:56:49 +0300

    feat: create LoginSchema

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -20,3 +20,8 @@ class UserPublicSchema(Schema):
 class OnboardDriverSchema(Schema):
     name = fields.Str(required=True, validate=validate.Length(min=1, max=128))
     phone = fields.Str(required=True, validate=validate.Length(min=8, max=20))
+
+    
+class LoginSchema(Schema):
+    identifier = fields.Str(required=True) # phone for drivers, email for managers
+    password = fields.Str(required=True)
