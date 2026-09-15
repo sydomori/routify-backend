3dac7f55f630889c1a11a2329b8aa791240584cc
sydomori <sydneyomori@gmail.com>
2026-09-15 15:37:26 +0300

    feat: create db alias

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -7,15 +7,23 @@ from app.auth.models import User
 from app.auth.utils import hash_password
 
 
-#set up temporary isolated testing environment
-@pytest.fixture()
+"""
+set up temporary isolated testing environment
+ensures every test runs in a blank db
+"""
+@pytest.fixture() #tells pytest the function is a set up tool
 def app():
     """Fresh app + in-memory DB per test function"""
-    application = create_app("testing")
+    application = create_app("testing") #app factory function from app/__init__.py used to create a testing environment
     with application.app_context():
         _db.create_all()
-        yield application
-        _db.session.remove()
-        _db.drop_all()
+        yield application #run the test
+        _db.session.remove() #close the db session. Clears out leftover database transactions
+        _db.drop_all() #deletes tables & data created from the test leaving the db empty
+
+
+@pytest.fixture()
+def db(app):
+    return _db
 
 
